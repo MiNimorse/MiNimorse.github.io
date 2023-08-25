4f151e7089f48eb912af1261e4eae347df1d3f4d
Nathakorn Paradeerujira <56668833+ColaBear101@users.noreply.github.com>
2023-08-25 12:24:50 +0700

    25/8

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 let morseString = '';
+let Point = 0;
 
 const morseButton = document.getElementById('morseButton');
 const collectedMorseSpan = document.getElementById('collectedMorse');
+const AlphabetHead = document.getElementById('AlphabetHead');
 
 let pressStartTime = 0;
-let pressEndTime = 0; // Initialize pressEndTime
 let pressTimer = null;
-const longPressDuration = 400; // Long press duration
-const timeOutDuration = 5000; // Clear time in
+const longPressDuration = 400;
+const timeOutDuration = 5000;
 
-// let autoDecodeTimer = null;
-// const autoDecodeTimeout = 3000;
-
-const Alphabetwant = ['A,B']
+const Alphabetwant = ['A', 'B'];
 
 function RandomAlpha() {
-    Math.random() * Alphabetwant.length;
+    let Randomlaw = Math.floor(Math.random() * Alphabetwant.length);
+    const AlphabetNow = Alphabetwant[Randomlaw];
 
+    return AlphabetNow;
 }
 
 function clearMorseString() {
@@ -24,43 +24,52 @@ function clearMorseString() {
     collectedMorseSpan.textContent = morseString;
 }
 
-function decodeMorse() {
-    const morseDecoder = require('morse-decoder');
-    const decodedMorseCode = morseDecoder.decode('morseString');
-    const decode = document.getElementById('Decoded');
-    decode.textContent = decodedMorseCode;
-}
-
 morseButton.addEventListener('mousedown', () => {
     pressStartTime = new Date().getTime();
     pressTimer = setTimeout(() => {
         morseString += '-';
         collectedMorseSpan.textContent = morseString;
-        clearTimeout(autoDecodeTimer); // Reset the auto decode timer
     }, longPressDuration);
 });
 
 morseButton.addEventListener('mouseup', () => {
-    clearTimeout(pressTimer); // Clear the press timer
-
-    // Calculate time from mouseup to detect long press timeout
-    const outStartTime = new Date().getTime();
-    const pressDuration = outStartTime - pressStartTime;
+    clearTimeout(pressTimer);
+    const pressEndTime = new Date().getTime();
+    const pressDuration = pressEndTime - pressStartTime;
 
     if (pressDuration <= longPressDuration) {
         morseString += '.';
         collectedMorseSpan.textContent = morseString;
-
-        clearTimeout(autoDecodeTimer); // Reset the auto decode timer
     }
 
-    autoDecodeTimer = setTimeout(() => {
-        decodeMorse();
-        clearMorseString();
-    }, autoDecodeTimeout);
+    // Implement auto-decode functionality
+
+    // Compare the entered morseString with the corresponding morse code
+    const currentAlphabet = AlphabetHead.textContent;
+    const morseDecoder = require('morse-decoder');
+    const morseAlphabet = morseDecoder.encode(currentAlphabet);
+
+    if (morseString === morseAlphabet) {
+        Point += 1;
+    }
+
+    // Clear the morseString and update the score
+    clearMorseString();
+    // Update the UI with the score
+
+    // Start a new round
+    startNewRound();
 });
+
+function startNewRound() {
+    const selectedAlphabet = RandomAlpha();
+    AlphabetHead.textContent = selectedAlphabet;
+}
 
 // Clear the Morse string after the specified timeout
 setTimeout(() => {
     clearMorseString();
 }, timeOutDuration);
+
+// Start the first round
+startNewRound();
